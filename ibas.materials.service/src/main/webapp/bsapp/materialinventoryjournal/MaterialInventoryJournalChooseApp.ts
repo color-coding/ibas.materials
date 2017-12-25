499cd4dfb2b8310431d57c18a397daadfc132c2b
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-25 12:01:46 +0800

    修复ChooseApp分页时自动选择问题

--- ibas.materials.service/src/main/webapp/bsapp/materialinventoryjournal/MaterialInventoryJournalChooseApp.ts
+++ ibas.materials.service/src/main/webapp/bsapp/materialinventoryjournal/MaterialInventoryJournalChooseApp.ts
@@ -51,7 +51,7 @@ export class MaterialInventoryJournalChooseApp extends ibas.BOChooseService<IMat
                         throw new Error(opRslt.message);
                     }
                     if (opRslt.resultObjects.length === 1
-                        && ibas.config.get(ibas.CONFIG_ITEM_AUTO_CHOOSE_DATA, true)) {
+                        && ibas.config.get(ibas.CONFIG_ITEM_AUTO_CHOOSE_DATA, true) && !that.isViewShowed()) {
                         // 仅一条数据，直接选择
                         that.chooseData(opRslt.resultObjects);
                     } else {
