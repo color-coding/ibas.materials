936866d023aa78a7bfedf42ce2bbda6c9d7f8265
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-05 11:25:31 +0800

    编辑对象时，新建对象直接使用

--- ibas.materials.service/src/main/webapp/bsapp/goodsissue/GoodsIssueEditApp.ts
+++ ibas.materials.service/src/main/webapp/bsapp/goodsissue/GoodsIssueEditApp.ts
@@ -77,8 +77,15 @@ export class GoodsIssueEditApp extends ibas.BOEditApplication<IGoodsIssueEditVie
     run(): void {
         let that: this = this;
         if (ibas.objects.instanceOf(arguments[0], bo.GoodsIssue)) {
+            let data: bo.GoodsIssue = arguments[0];
+            // 新对象直接编辑
+            if (data.isNew) {
+                that.editData = data;
+                that.show();
+                return;
+            }
             // 尝试重新查询编辑对象
-            let criteria: ibas.ICriteria = arguments[0].criteria();
+            let criteria: ibas.ICriteria = data.criteria();
             if (!ibas.objects.isNull(criteria) && criteria.conditions.length > 0) {
                 // 有效的查询对象查询
                 let boRepository: BORepositoryMaterials = new BORepositoryMaterials();
--- ibas.materials.service/src/main/webapp/bsapp/materialgroup/MaterialGroupEditApp.ts
+++ ibas.materials.service/src/main/webapp/bsapp/materialgroup/MaterialGroupEditApp.ts
@@ -50,8 +50,15 @@ export class MaterialGroupEditApp extends ibas.BOEditApplication<IMaterialGroupE
     run(): void {
         let that: this = this;
         if (ibas.objects.instanceOf(arguments[0], bo.MaterialGroup)) {
+            let data: bo.MaterialGroup = arguments[0];
+            // 新对象直接编辑
+            if (data.isNew) {
+                that.editData = data;
+                that.show();
+                return;
+            }
             // 尝试重新查询编辑对象
-            let criteria: ibas.ICriteria = arguments[0].criteria();
+            let criteria: ibas.ICriteria = data.criteria();
             if (!ibas.objects.isNull(criteria) && criteria.conditions.length > 0) {
                 // 有效的查询对象查询
                 let boRepository: BORepositoryMaterials = new BORepositoryMaterials();
