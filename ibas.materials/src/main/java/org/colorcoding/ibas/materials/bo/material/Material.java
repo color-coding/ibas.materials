b10fb7c382c192b937787b48407d18963df33e1f
niurenzhu <niuren.zhu@icloud.com>
2018-03-30 21:55:00 +0800

    添加标记删除接口

--- ibas.materials/src/main/java/org/colorcoding/ibas/materials/bo/material/Material.java
+++ ibas.materials/src/main/java/org/colorcoding/ibas/materials/bo/material/Material.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.colorcoding.ibas.bobas.approval.IApprovalData;
 import org.colorcoding.ibas.bobas.bo.IBOSeriesKey;
+import org.colorcoding.ibas.bobas.bo.IBOTagDeleted;
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.Decimal;
@@ -30,7 +31,8 @@ import org.colorcoding.ibas.materials.data.emItemType;
 @XmlType(name = Material.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlRootElement(name = Material.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @BOCode(Material.BUSINESS_OBJECT_CODE)
-public class Material extends MaterialBase<Material> implements IMaterial, IDataOwnership, IApprovalData, IBOSeriesKey {
+public class Material extends MaterialBase<Material>
+		implements IMaterial, IDataOwnership, IApprovalData, IBOSeriesKey, IBOTagDeleted {
 
 	/**
 	 * 序列化版本标记
