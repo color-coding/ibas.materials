b8ca84dba1f8284e813a368a400c209c2ae64d39
niurenzhu <niuren.zhu@icloud.com>
2018-06-27 23:39:41 +0800

    更新配置机制

--- ibas.materials/src/main/java/org/colorcoding/ibas/materials/MyConfiguration.java
+++ ibas.materials/src/main/java/org/colorcoding/ibas/materials/MyConfiguration.java
@@ -1,9 +1,36 @@
 package org.colorcoding.ibas.materials;
 
+import org.colorcoding.ibas.bobas.configuration.ConfigurationFactory;
+import org.colorcoding.ibas.bobas.configuration.IConfigurationManager;
+
 /**
  * 我的配置项
  */
 public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration {
+
+	private volatile static IConfigurationManager instance;
+
+	public static IConfigurationManager create() {
+		if (instance == null) {
+			synchronized (MyConfiguration.class) {
+				if (instance == null) {
+					instance = ConfigurationFactory.create().createManager();
+					instance.setConfigSign(MODULE_ID);
+					instance.update();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public static <P> P getConfigValue(String key, P defaultValue) {
+		return create().getConfigValue(key, defaultValue);
+	}
+
+	public static String getConfigValue(String key) {
+		return create().getConfigValue(key);
+	}
+
 	/**
 	 * 模块标识
 	 */
@@ -28,10 +55,6 @@ public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration 
 	 * 服务命名空间
 	 */
 	public static final String NAMESPACE_SERVICE = NAMESPACE_ROOT + "service";
-	/**
-	 * 配置项目-文件文件夹
-	 */
-	public final static String CONFIG_ITEM_MATERIALS_FILE_FOLDER = "MMFileFolder";
 
 	/** 配置项目-默认货币 */
 	public final static String CONFIG_ITEM_DEFAULT_CURRENCY = "defaultCurrency";
