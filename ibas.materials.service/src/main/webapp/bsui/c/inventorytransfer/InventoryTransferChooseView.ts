d7078f0d69deea63f163e84b9821297b85864bb7
牛加人等于朱 <niuren.zhu@icloud.com>
2018-09-06 15:55:06 +0800

    优化交互

--- ibas.materials.service/src/main/webapp/bsui/c/inventorytransfer/InventoryTransferChooseView.ts
+++ ibas.materials.service/src/main/webapp/bsui/c/inventorytransfer/InventoryTransferChooseView.ts
@@ -150,6 +150,7 @@ namespace materials {
                         buttons: [
                             new sap.m.Button("", {
                                 text: ibas.i18n.prop("shell_data_new"),
+                                visible: this.mode === ibas.emViewMode.VIEW ? false : true,
                                 type: sap.m.ButtonType.Transparent,
                                 // icon: "sap-icon://create",
                                 press: function (): void {
