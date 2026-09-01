color-coding/ibas.receiptpayment

优化编辑及查看应用数据加载

ibas.receiptpayment.service/src/main/webapp/bsapp/Console.ts
@@ -31,6 +31,7 @@ namespace receiptpayment {
                 this.register(new AssetRechargeFunc());
                 this.register(new InternalReconciliationFunc());
                 // 注册服务应用
+                this.register(new InternalReconciliationEditServiceMapping());
                 this.register(new PaymentChooseServiceMapping());
                 this.register(new PaymentLinkServiceMapping());
                 this.register(new PaymentEditServiceMapping());

color-coding/ibas.materials

ibas.materials.service/src/main/webapp/bsapp/Console.ts
@@ -56,6 +56,17 @@ namespace materials {
                 // this.register(new MaterialGroupFunc());
                 // this.register(new UnitFunc());
                 // 注册服务应用
+                this.register(new UnitEditServiceMapping());
+                this.register(new MaterialVersionEditServiceMapping());
+                this.register(new InventoryTransferRequestEditServiceMapping());
+                this.register(new SchedulingGroupEditServiceMapping());
+                this.register(new MaterialGroupEditServiceMapping());
+                this.register(new SpecificationEditServiceMapping());
+                this.register(new MaterialSpecificationEditServiceMapping());
+                this.register(new MaterialScrapEditServiceMapping());
+                this.register(new InventoryCountingEditServiceMapping());
+                this.register(new WarehouseEditServiceMapping());
+                this.register(new MaterialEditServiceMapping());
                 this.register(new MaterialChooseServiceMapping());
                 this.register(new MaterialLinkServiceMapping());
                 this.register(new MaterialGroupChooseServiceMapping());