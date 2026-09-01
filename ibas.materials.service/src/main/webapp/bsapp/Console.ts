color-coding/ibas.initialfantasy

Monthly merge: master -> v0.2.x (2026-08.1)

ibas.initialfantasy.service/src/main/webapp/bsapp/Console.ts
@@ -50,6 +50,17 @@ namespace initialfantasy {
                     // this.register(new BOInformationFunc());
                 }
                 // 注册服务应用
+                this.register(new OrganizationEditServiceMapping());
+                this.register(new IdentityEditServiceMapping());
+                this.register(new PrivilegeEditServiceMapping());
+                this.register(new BOFilteringEditServiceMapping());
+                this.register(new UserEditServiceMapping());
+                this.register(new BOCriteriaEditServiceMapping());
+                this.register(new BOInformationEditServiceMapping());
+                this.register(new UserIdentityEditServiceMapping());
+                this.register(new ApplicationPlatformEditServiceMapping());
+                this.register(new ApplicationElementEditServiceMapping());
+                this.register(new ApplicationModuleEditServiceMapping());
                 this.register(new ApplicationElementChooseServiceMapping());
                 this.register(new ApplicationModuleChooseServiceMapping());
                 this.register(new ApplicationPlatformChooseServiceMapping());

color-coding/ibas.materials

ibas.materials.service/src/main/webapp/bsapp/Console.ts
@@ -56,6 +56,17 @@ namespace materials {
                 // this.register(new MaterialGroupFunc());
                 // this.register(new UnitFunc());
                 // 注册服务应用
+                this.register(new UnitEditServiceMapping());
+                this.register(new MaterialVersionEditServiceMapping());
+                this.register(new InventoryTransferRequestEditServiceMapping());
+                this.register(new SchedulingGroupEditServiceMapping());
+                this.register(new MaterialGroupEditServiceMapping());
+                this.register(new SpecificationEditServiceMapping());
+                this.register(new MaterialSpecificationEditServiceMapping());
+                this.register(new MaterialScrapEditServiceMapping());
+                this.register(new InventoryCountingEditServiceMapping());
+                this.register(new WarehouseEditServiceMapping());
+                this.register(new MaterialEditServiceMapping());
                 this.register(new MaterialChooseServiceMapping());
                 this.register(new MaterialLinkServiceMapping());
                 this.register(new MaterialGroupChooseServiceMapping());